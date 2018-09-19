87304c5c5bf9f3c9e89138e6af2d3dca7cc348f2
ellen <ellen@10.104.43.123>
2018-09-19 08:24:15 -0700

    Created project

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -14,8 +14,78 @@
  *
  */
 
+
+//Unit 1 Project
+//AP Computer Science, P2
+
 public class ASCIIArt {
-	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
+	
+	public static void stick(int h) { //the stick sticking out of the top of a lighthouse; int h determines its height
+		for(int j = 0; j < h; j++) {
+			for (int i = 0; i < 6; i++) {
+				System.out.print(" ");
+			}
+			System.out.print("||\n");
+		}
+	}
+	
+	public static void triangle() { //triangle roof
+		System.out.println("      /\\");
+		System.out.println("     /  \\");
+		System.out.println("    /    \\");
+		System.out.println("   /      \\");
+	}
+	
+	public static void smallestRoof(int width) { //how wide the smallest roof is.
+		for (int j = 0; j < 3; j++) {
+			
+			for (int i = 0; i < width/4 ; i++) { //spaces before left side of smallest roof
+				System.out.print(" ");
+			}
+			
+			System.out.print("|");
+			for (int i = 0; i < width; i++) { //actual roof
+				System.out.print("=");
+			}
+			System.out.print("|\n");
+		}
+	}
+		
+	
+	public static void bigRoof(int width) {
+		for (int i = 0; i < width; i++) {
+			System.out.print("[]");
+			}
+		System.out.println();
+		}
+	
+	public static void body() {
+		System.out.println("  |        |");
+	}
+	
+	public static void windowHorizontal() {
+		System.out.println("  | ------ |");
+	}
+	
+	public static void main (String[] args) {
+		stick(2);
+		triangle();
+		smallestRoof(8);
+		bigRoof(7);
+		bigRoof(7);
+		body();
+		windowHorizontal();
+		System.out.println("  | |    | |");
+		windowHorizontal();
+		
+		for(int i = 0; i < 5; i++) {
+			body();
+		}
+		
+		for (int g = 0; g < 16; g++) {
+			System.out.print("^");
+		}
+		
+		System.out.println("\nWow. This is my lighthouse.");
 	}
 }
